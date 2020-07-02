f6ddb12991b360089a3fa67ead503f188c40a630
Akash Sharma <akash1991@gmail.com>
2020-07-03 04:56:36 +0530

    Updates

---
+++ precache-manifest.a98f66fb10fdb9c33527fbcb153e3622.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f76b4adbcb826176c804c5ba8890a400",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a87a6eebe58247537a01",
+    "url": "/static/css/main.32e5179f.chunk.css"
+  },
+  {
+    "revision": "555fc9a9189dfa3a25d8",
+    "url": "/static/js/2.c05ef38b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c05ef38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a87a6eebe58247537a01",
+    "url": "/static/js/main.b84cd73b.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  },
+  {
+    "revision": "653133f4046cdb908d5d09cfed2706cb",
+    "url": "/static/media/noun_poop.653133f4.svg"
+  }
+]);
