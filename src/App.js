13f4cabdeb84976f0330392871f3d109d75f6d91
Akash Sharma <akash1991@gmail.com>
2020-07-13 06:24:16 +0530

    Hotjar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import './App.css';
-import LandingPage from './components/LandingPage';
-import Header from './components/Header';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import LandingPage from "./components/LandingPage";
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <LandingPage />
-      {/* <Footer /> */}
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/coming-soon">{/* <ComingSoon /> */}</Route>
+          <Route path="/" />
+        </Switch>
+        {/* <Footer /> */}
+      </div>
+    </Router>
   );
 }
 
